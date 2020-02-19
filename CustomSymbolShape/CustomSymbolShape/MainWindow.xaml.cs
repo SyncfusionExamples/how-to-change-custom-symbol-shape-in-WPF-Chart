09cc2ff3efda47b7737673698580234fabfe327f
Lavanya A <lavanya.anaimuthu@syncfusion.com>
2020-02-19 16:51:48 +0530

    Sample Added.

---
+++ CustomSymbolShape/CustomSymbolShape/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomSymbolShape
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class SymbolDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate HighValueTemplate { get; set; }
+        public DataTemplate LowValueTemplate { get; set; }
+        public DataTemplate EmptyValueTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null) return EmptyValueTemplate;
+
+            var adornment = item as ChartAdornment;
+
+            var model = adornment.Item as Model;
+
+            if (model.YValue < 20)
+            {
+                return HighValueTemplate;
+            }
+            else
+            {
+                return LowValueTemplate;
+            }
+        }
+    }
+
+}
---
+++ CustomSymbolShape/CustomSymbolShape/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomSymbolShape
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>()
+            {
+                new Model() {XValue = 0, YValue= 30},
+                new Model() {XValue = 1, YValue= 40},
+                new Model() {XValue = 2, YValue= 15},
+                new Model() {XValue = 3, YValue= 45},
+                new Model() {XValue = 4, YValue= 25},
+                new Model() {XValue = 5, YValue= 30},
+                new Model() {XValue = 6, YValue= 13},
+                new Model() {XValue = 7, YValue= 10},
+                new Model() {XValue = 8, YValue= 30},
+            };
+
+        }
+    }
+
+    public class Model
+    {
+        public double XValue { get; set; }
+
+        public double YValue { get; set; }
+
+    }
+}
